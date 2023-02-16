abca8c03dd9bf5a32a7977473d1ef264f422fca8
Eric ⚔️ <dont-email@ignight.dev>
2023-02-15 18:48:42 -0800

    Wed Feb 15 18:48:42 PST 2023

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,47 @@
-export default function App()  {
-    return <div>
-        test
+import { useState } from "react";
+
+function PageWrapper( props : { children: React.ReactNode } ) {
+    return <div className="page-wrapper">
+        <div> Welcome to HELLPful react tutorial</div>
+        {props.children}
     </div>
 }
 
+function InputBox() {
+    const [inputState, setInputState] = useState('')
+    return <div>
+        <div>{inputState}</div>
+        <input 
+            type="text"
+            value={inputState}
+            onChange={e => setInputState(e.target.value)}
+        />
+    </div>
+}
+
+export default function App()  {
+
+    // Create button to increment state2
+    const [state2, setState2] = useState(213);
+
+    let myValues = []
+
+    for (let i = 0; i < state2; i++) {
+        myValues.push(
+            <div> {i} </div>
+        )
+    }
+
+    const button = <div>
+        <div>{state2}</div>
+        <button onClick={() => setState2(state2 + 1)}>Increment</button>
+        {  myValues.reverse() }
+    </div>
+
+    return <PageWrapper>
+        <div className="test-class">
+            <InputBox />
+            {button}
+        </div>
+    </PageWrapper>
+}
